0f504eaf6b2bbb609bfe09fa010d252a4432c338
0leg-zharkov <60039706+0leg-zharkov@users.noreply.github.com>
2021-10-18 16:01:14 +0300

    Сделан метод нажатия на умножение

--- WpfGitDz/MainWindow.xaml.cs
+++ WpfGitDz/MainWindow.xaml.cs
@@ -47,6 +47,13 @@ namespace WpfGitDz
             LabelNumbers.Content = "";
         }
 
+        private void Button_Click_Umn(object sender, RoutedEventArgs e)
+        {
+            number1 = Int32.Parse(LabelNumbers.Content.ToString());
+            symbol = '*';
+            LabelNumbers.Content = "";
+        }
+
         private void Button_Click_Res(object sender, RoutedEventArgs e)
         {
             number2 = Int32.Parse(LabelNumbers.Content.ToString());
